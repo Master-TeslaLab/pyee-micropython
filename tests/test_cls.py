f9484f14a65b3305b5106916b7e24a4dc368aae5
Master-TeslaLab <yjsong@teslalab.kr>
2024-12-25 15:03:38 +0900

    test: Add test code with mpy-unix-devenv

--- tests/test_cls.py
+++ tests/test_cls.py
@@ -1,48 +1,18 @@
-# -*- coding: utf-8 -*-
-from unittest.mock import Mock
-
-import pytest
-
-from pyee import EventEmitter
+import sys
+sys.path.append('/workspace')
+from pyee import *
 from pyee.cls import evented, on
 
-
 @evented
-class EventedFixture:
-    def __init__(self):
-        self.call_me = Mock()
-
+class Evented:
     @on("event")
     def event_handler(self, *args, **kwargs):
-        self.call_me(self, *args, **kwargs)
+        print(self, args, kwargs)
 
+evented_obj = Evented()
 
-_custom_event_emitter = EventEmitter()
-
-
-@evented
-class CustomEmitterFixture:
-    def __init__(self):
-        self.call_me = Mock()
-        self.event_emitter = _custom_event_emitter
-
-    @on("event")
-    def event_handler(self, *args, **kwargs):
-        self.call_me(self, *args, **kwargs)
-
-
-class InheritedFixture(EventedFixture):
-    pass
-
-
-@pytest.mark.parametrize(
-    "cls", [EventedFixture, CustomEmitterFixture, InheritedFixture]
+evented_obj.event_emitter.emit(
+    "event", "hello world", numbers=[1, 2, 3]
 )
-def test_evented_decorator(cls):
-    inst = cls()
 
-    inst.event_emitter.emit("event", "emitter is emitted!")
-
-    inst.call_me.assert_called_once_with(inst, "emitter is emitted!")
-
-    _custom_event_emitter.remove_all_listeners()
+help(evented_obj)
